391f82e84378e3d0f9c09eb942466ab364b80b0e
Tanatip Boonloy <Tanatip Boonloy>
2017-03-24 01:28:50 +0700

    init project

---
+++ src/main/java/kmitl/sp/smp/SmartMusicPlayerServiceApplication.java
@@ -0,0 +1,12 @@
+package kmitl.sp.smp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMusicPlayerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMusicPlayerServiceApplication.class, args);
+	}
+}
